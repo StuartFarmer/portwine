9d27e16ace7411dbf4b2b838a38468974bf46efe
Stuart Farmer <stuart@lamden.io>
2025-04-14 23:40:27 -0400

    new analyzers and noisy data loader

--- portwine/loaders/__init__.py
+++ portwine/loaders/__init__.py
@@ -1,3 +1,4 @@
 from portwine.loaders.base import MarketDataLoader
 from portwine.loaders.eodhd import EODHDMarketDataLoader
 from portwine.loaders.polygon import PolygonMarketDataLoader
+from portwine.loaders.noisy import NoisyMarketDataLoader
